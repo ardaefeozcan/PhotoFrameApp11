08413f00fe2b773fda9a302bf896ff2df2493373
ardaefeozcan <ardaefe.ozcan@hisarschool.k12.tr>
2024-09-04 01:24:04 +0300

    Initial Commit

---
+++ PhotoFrameApp11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrameApp11
+//
+//  Created by Arda Efe Özcan on 4.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
